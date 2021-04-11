bb51e20eb96991b11d0f288402fe4eaba22697b4
asrytikov <asrytikov@gmail.com>
2021-04-11 15:32:16 +0400

    les10-2

--- script/script.js
+++ script/script.js
@@ -9,4 +9,18 @@ $(document).ready(function(){
     $('.accordion').accordion({
         defaultOpen:''
     });
+    $(function(){
+        let filterlist = {
+            init: function(){
+                $('.workportfolio').mixitup({
+                    targetSelection: '.portfolio',
+                    filterSelection: '.filter',
+                    effects: ['fade'],
+                    easing: 'snap'
+                });
+            },
+        };
+        filterlist.init();
+    });
+
 });
